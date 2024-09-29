90e4824d3c34a509286a02f9c7ba61a004754d84
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-28 17:21:03 -0700

    Section added

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,23 @@ function App() {
     <Authenticator>
       {({ signOut }) => (
     <main>
+
+
+ <section>
+  <nav>
+    <ul>
+      <li><a href="#">London</a></li>
+      <li><a href="#">Paris</a></li>
+      <li><a href="#">Tokyo</a></li>
+    </ul>
+  </nav>
   
+  <article>
+    <h1>London</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article>
+</section>
       
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -33,13 +49,9 @@ function App() {
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a> 
-      </div>
+
+
+      
                 <button onClick={signOut}>Sign out</button>
     </main>
           
