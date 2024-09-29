343c0eacb660894ba65adf9ea29fdc29ad6877e5
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-28 17:13:49 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,11 @@ function App() {
         ))}
       </ul>
       <div>
-          <!-- 🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
-        </a> -->
+        </a> 
       </div>
                 <button onClick={signOut}>Sign out</button>
     </main>
