b46639b8edd758770037e4b665314f2a731a220d
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-29 17:00:52 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import App from "./App.tsx";
 import App from "./App.jsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
