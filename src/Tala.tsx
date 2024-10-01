64c0679aac98d90ec1ce721aa6f25eecf9b78e8e
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-30 21:16:51 -0700

    Create Tala.tsx

---
+++ src/Tala.tsx
@@ -0,0 +1,13 @@
+// Tala.tsx
+import React from "react";
+
+const Tala: React.FC = () => {
+  return (
+    <div>
+      <h1>Tala Page</h1>
+      <p>Content for the Tala page goes here.</p>
+    </div>
+  );
+};
+
+export default Tala;
