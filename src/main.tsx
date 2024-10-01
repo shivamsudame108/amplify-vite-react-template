63f6cce71714690cf477d2207c2f442e42c587ac
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-30 21:23:33 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+//import App from "./App.jsx";
+import Menu from "./Menu"; // Change to import Menu
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
@@ -9,6 +11,8 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Menu /> {/* Change App to Menu */}
+    </BrowserRouter>
   </React.StrictMode>
 );
