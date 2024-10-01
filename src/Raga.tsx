1ef907159aa1a6381fb30f773a8587ecaee82a6a
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-30 21:16:31 -0700

    Create Raga.tsx

---
+++ src/Raga.tsx
@@ -0,0 +1,13 @@
+// Raga.tsx
+import React from "react";
+
+const Raga: React.FC = () => {
+  return (
+    <div>
+      <h1>Raga Page</h1>
+      <p>Content for the Raga page goes here.</p>
+    </div>
+  );
+};
+
+export default Raga;
