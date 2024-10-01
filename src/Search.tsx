a8383d5bfa8e208abed175e206f3141db77caa85
shivamsudame108 <shivamsudame108@gmail.com>
2024-09-30 21:17:22 -0700

    Create Search.tsx

---
+++ src/Search.tsx
@@ -0,0 +1,13 @@
+// Search.tsx
+import React from "react";
+
+const Search: React.FC = () => {
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <p>Content for the Search page goes here.</p>
+    </div>
+  );
+};
+
+export default Search;
